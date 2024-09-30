ed18df3515127b2dd6d542b61f1d2bfc8cb7c4b0
Chwakillah <syakillahnachwa081@gmail.com>
2024-09-30 22:00:39 +0700

    fix(detail): memperbaiki navigasi

--- app/src/main/java/com/belajar/finalprojectandroidbeginner/AboutActivity.kt
+++ app/src/main/java/com/belajar/finalprojectandroidbeginner/AboutActivity.kt
@@ -1,6 +1,8 @@
 package com.belajar.finalprojectandroidbeginner
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 
@@ -9,5 +11,10 @@ class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        val ivBtnBack = findViewById<ImageView>(R.id.iv_btnBack)
+        ivBtnBack.setOnClickListener{
+            finish()
+        }
     }
 }
